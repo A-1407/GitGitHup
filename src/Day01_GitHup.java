888099881e4b4c9fa8320980b24a2018717c1fa7
01Furkan <furkanaydgn44>
2023-11-16 22:27:19 +0300

    G1

---
+++ src/Day01_GitHup.java
@@ -0,0 +1,27 @@
+public class Day01_GitHup {
+    public static void main(String[] args) {
+
+        /*
+        1] git init --> Local repo olusturmak icin yani .git ile klasorumuzun icindeki dosyalari
+        iliskilendirmek icin kullanilir
+
+        2} git add . --> working spac'den yani (yerel depomuzdan) staging areá'ya (commitlemek icin beklenen yer)
+        gonderir
+
+        3} git status --> working  space'deki ve staging area'daki durumu gosterir
+
+        4} git commit -m "mesaj" --> staging area'dan commit store gondermek icin kullanilir
+        (commit yani versiyon yani surum olusturmus oluruz)
+
+        5} git push -> Uzak repo'ya - GitHup'a gonderdigimiz komut
+        Yalniz git push komudunu direk kullanmak istersek bir Kez;
+                       git remote add origin adress
+                       git push -u origin master
+        Not;Bu iki komutu tek seferde kullandiktan sonra ikinci commitlerim icin bir defa
+        sadece --git push--
+
+        Not; git log --oneline --> Commitlerdeki yani versiyonlarimdaki durumunu gosterir
+         */
+        System.out.println("GitHup'da Ogrendim");
+    }
+}
